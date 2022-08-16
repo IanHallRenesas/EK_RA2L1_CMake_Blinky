26495d19b86454fef6f33ffc4af36a7a8f4ea8fa
Ian Hall <ian.hall@renesas.com>
2022-08-16 15:07:33 +0100

    Initial commit
    Example (with instructions PDF) of using RASC to create a CMake project.

---
+++ Config.cmake
@@ -0,0 +1,4 @@
+# Configuration file for user settings
+# This file should include the path for toolchain and other settings that user would like to override.
+# Example toolchain path definitions
+set(CMAKE_FIND_ROOT_PATH "C:/Renesas/RA/e2studio_v2022-04_fsp_v3.7.0/toolchains/gcc_arm/gcc-arm-none-eabi-10.3-2021.10/bin")
---
+++ src/hal_entry.c.d
@@ -0,0 +1,104 @@
+CMakeFiles/EK_RA2L1_Blinky_CMake.elf.dir/src/hal_entry.c.obj: \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/src/hal_entry.c \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_gen/hal_data.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/fsp/inc/api/bsp_api.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/fsp/inc/fsp_common_api.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/fsp/inc/fsp_version.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_cfg/fsp_cfg/bsp/bsp_cfg.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_gen/bsp_clock_cfg.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\ra2l1\bsp_mcu_info.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\ra2l1\bsp_elc.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\ra2l1\bsp_icu.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\ra2l1\bsp_feature.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\ra2l1\bsp_power.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_cfg/fsp_cfg/bsp/board_cfg.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\board\ra2l1_ek\board.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\board\ra2l1_ek\board_init.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\board\ra2l1_ek\board_leds.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_arm_exceptions.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_gen/vector_data.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\cmsis\device\renesas\include\renesas.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/arm/CMSIS_5/CMSIS/Core/Include/core_cm23.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_version.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/arm/CMSIS_5/CMSIS/Core/Include/mpu_armv8.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\cmsis\device\renesas\include\base_addresses.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_common.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\inc\fsp_common_api.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_register_protection.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_irq.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_io.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_group_irq.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_clocks.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_module_stop.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_security.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\inc\fsp_features.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\inc\fsp_common_api.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_delay.h \
+ c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_mcu_api.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_gen/common_data.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/fsp/inc/instances/r_ioport.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/fsp/inc/api/r_ioport_api.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/fsp/inc/api/bsp_api.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_cfg/fsp_cfg/r_ioport_cfg.h \
+ C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_cfg/fsp_cfg/bsp/bsp_pin_cfg.h
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_gen/hal_data.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/fsp/inc/api/bsp_api.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/fsp/inc/fsp_common_api.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/fsp/inc/fsp_version.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_cfg/fsp_cfg/bsp/bsp_cfg.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_gen/bsp_clock_cfg.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\ra2l1\bsp_mcu_info.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\ra2l1\bsp_elc.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\ra2l1\bsp_icu.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\ra2l1\bsp_feature.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\ra2l1\bsp_power.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_cfg/fsp_cfg/bsp/board_cfg.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\board\ra2l1_ek\board.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\board\ra2l1_ek\board_init.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\board\ra2l1_ek\board_leds.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_arm_exceptions.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_gen/vector_data.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\cmsis\device\renesas\include\renesas.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/arm/CMSIS_5/CMSIS/Core/Include/core_cm23.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_version.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/arm/CMSIS_5/CMSIS/Core/Include/mpu_armv8.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\cmsis\device\renesas\include\base_addresses.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_common.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\inc\fsp_common_api.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_register_protection.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_irq.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_io.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_group_irq.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_clocks.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_module_stop.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_security.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\inc\fsp_features.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\inc\fsp_common_api.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_delay.h:
+c:\work\ra\fsp_3_8_0\ek_ra2l1_blinky_cmake\ra\fsp\src\bsp\mcu\all\bsp_mcu_api.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_gen/common_data.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/fsp/inc/instances/r_ioport.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/fsp/inc/api/r_ioport_api.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra/fsp/inc/api/bsp_api.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_cfg/fsp_cfg/r_ioport_cfg.h:
+C:/Work/RA/FSP_3_8_0/EK_RA2L1_Blinky_CMake/ra_cfg/fsp_cfg/bsp/bsp_pin_cfg.h:
